41cd9f457329bf4b7ce99c6108af8886c4d338aa
Ted Hudek <tedhudek@gmail.com>
2017-07-24 16:33:45 -0700

    added comments

--- src/extension.ts
+++ src/extension.ts
@@ -9,11 +9,13 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "poplink" is now active!');
+    console.log('Congrats, your extension "poplink" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+
+    // For next version consider an alternative to hardcoded values
     let disposable = vscode.commands.registerCommand('extension.poplink', () => {
         // The code you place here will be executed every time your command is executed
 
