911a7869a6b656ed5dc7010196a0a4ca34f5518b
Ted Hudek <tedhudek@gmail.com>
2017-07-25 15:30:37 -0700

    comment updates

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
 
-    // For next version consider an alternative to hardcoded values
     let disposable = vscode.commands.registerCommand('extension.poplink', () => {
         // The code you place here will be executed every time your command is executed
 
